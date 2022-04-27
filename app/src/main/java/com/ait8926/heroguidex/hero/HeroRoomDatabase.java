cfb7fcf7ec9317d6e04802638b364fa087f2b59d
Mike <graphicalevolution@gmail.com>
2022-04-28 00:40:07 +1000

    Populate additional 5 heroes in the database

--- app/src/main/java/com/ait8926/heroguidex/hero/HeroRoomDatabase.java
+++ app/src/main/java/com/ait8926/heroguidex/hero/HeroRoomDatabase.java
@@ -59,6 +59,7 @@ public abstract class HeroRoomDatabase  extends RoomDatabase {
         }
     };
 
+
     // This will initialise declared data examples only. this is to populate data when we start the app
     private static void populateInitialData(HeroRoomDatabase instance) {
         //we execute database operations in threads
@@ -74,9 +75,24 @@ public abstract class HeroRoomDatabase  extends RoomDatabase {
                     "one of the highest physical damage output ceilings of all heroes.", "Carry", "hero_3", "hero3_audio"));
             heroDao.insert(new Hero(55, 88, 72, 3, 3, "GrimStroke", "Grimstroke is a ranged intelligence hero " +
                     "specializing as a nuker and offensive support.", "Support", "hero_4", "hero4_audio"));
-            heroDao.insert(new Hero(80, 99,60,5,2,"Juggernaut","Yurnero, the Juggernaut, is a melee agility hero whose abilities " +
-                    "allow him to sprint into battle and recklessly devastate enemies " +
-                    "in an impenetrable flurry of blades.", "Carry", "hero_5", "hero5_audio"));
+            heroDao.insert(new Hero(80, 99,60,5,2,"Juggernaut","No one has ever seen the face hidden beneath the mask of Yurnero the Juggernaut. " +
+                    "It is only speculation that he even has one. For defying a corrupt lord, Yurnero was exiled from the ancient Isle of Masks--a punishment that saved his life. " +
+                    "The isle soon after vanished beneath the waves in a night of vengeful magic. He alone remains to carry on the Isle's long Juggernaut tradition, one of ritual and swordplay.", "Carry", "hero_5", "hero5_audio"));
+            heroDao.insert(new Hero(70, 80,50,9,4,"Shadow Fiend","It is said that Nevermore the Shadow Fiend has the soul of a poet, " +
+                    "and in fact he has thousands of them. Over the ages he has claimed the souls of poets, priests, emperors, beggars, slaves, philosophers, criminals and " +
+                    "(naturally) heroes; no sort of soul escapes him.", "Carry", "hero_6", "hero6_audio"));
+            heroDao.insert(new Hero(85, 90,60,10,5,"Phantom Assasin","Through a process of divination, children are selected " +
+                    "for upbringing by the Sisters of the Veil, an order that considers assassination a sacred part of the natural order. " +
+                    "The Veiled Sisters identify targets through meditation and oracular utterances.", "Carry", "hero_7", "hero7_audio"));
+            heroDao.insert(new Hero(62, 65,55,9,4,"Puck","While Puck seems at first glance a mischievous, childish character, " +
+                    "this quality masks an alien personality. The juvenile form of a Faerie Dragon, a creature that lives for eons, Puck spends countless millennia in its childish form. " +
+                    "So while it is technically true that Puck is juvenile, it will continue to be so when the cities of the present age have sloughed away into dust.", "Initiator", "hero_8", "hero8_audio"));
+            heroDao.insert(new Hero(72, 50,90,10,4,"Pudge","In the Fields of Endless Carnage, far to the south of Quoidge, a corpulent figure works tirelessly through the night--dismembering, " +
+                    "disembowelling, piling up the limbs and viscera of the fallen that the battlefield might be clear by dawn. " +
+                    "In this cursed realm, nothing can decay or decompose; no corpse may ever return to the earth from which it sprang, no matter how deep you dig the grave.", "Initiator", "hero_9", "hero9_audio"));
+            heroDao.insert(new Hero(90, 75,84,9,5,"Wraith King","For untold years, King Ostarion built a kingdom from the remains of his enemies. " +
+                    "It was an obsessive's errand, done to pass the long eternities of a monarchy that seemed fated never to end. He believed that as long as he built up the towers of his palace, he could not die. " +
+                    "But eventually he learned that he had been deluded... that bone itself could perish.", "Carry", "hero_10", "hero10_audio"));
         });
 
     }
